d597181b244c4d3ea6617aa30d9df931a64d4aec
Steve <Steve@S3>
2018-11-01 17:05:03 +0100

    commit 1

---
+++ Notlage/src/main/java/faeteam3/Notlage/NotlageApplication.java
@@ -0,0 +1,12 @@
+package faeteam3.Notlage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotlageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotlageApplication.class, args);
+	}
+}
